27f1b4cda2673f5715452178cd33cc1b538cb1c3
jae kap <me@jkap.io>
2017-08-18 11:04:46 -0700

    i'm on that pylint shit

--- search.py
+++ search.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
 
-import requests
-from bs4 import BeautifulSoup
-from twilio.rest import Client
-from dotenv import load_dotenv, find_dotenv
+"""Search for available FP+ times for a given ride."""
+
 import os
 import re
 import io
 from datetime import datetime
+import requests
+from bs4 import BeautifulSoup
+from twilio.rest import Client
+from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv())
 
@@ -15,62 +17,83 @@ RIDE = os.environ['RIDE']
 PARK = os.environ['PARK']
 DATE = os.environ['DATE']
 
-print("""STARTING SEARCH
-Park: %s
-Ride: %s
-Date: %s
-Current time: %s""" % (PARK, RIDE, DATE, str(datetime.now())))
 
-r = requests.get(
-    "https://touringplans.com/%s/fastpass-availability/date/%s" %
-    (PARK, DATE))
+def fetch_avail():
+    """Fetch and return available time list for RIDE from touringplans.com"""
+    req = requests.get(
+        "https://touringplans.com/%s/fastpass-availability/date/%s" %
+        (PARK, DATE))
 
-html_doc = r.text
+    html_doc = req.text
 
-soup = BeautifulSoup(html_doc, 'html.parser')
+    soup = BeautifulSoup(html_doc, 'html.parser')
 
-for elem in soup(text=re.compile(RIDE)):
-    el = elem
+    for elem in soup(text=re.compile(RIDE)):
+        ride_el = elem
 
-for parent in el.parents:
-    if parent.name == "tr":
-        p = parent
-        break
+    for parent in ride_el.parents:
+        if parent.name == "tr":
+            fp_row = parent
+            break
 
-fp = p.find_all("td")[1].text.strip()
+    return fp_row.find_all("td")[1].text.strip()
 
-print(fp)
 
-try:
-    f = io.open("prev/%s.txt" % (RIDE), "r", encoding='utf8')
-except FileNotFoundError:
-    prev = ""
-else:
-    prev = f.read()
-    f.close()
+def did_change(avail):
+    """Check cache and return if available times changed."""
+    try:
+        file = io.open("prev/%s.txt" % (RIDE), "r", encoding='utf8')
+    except FileNotFoundError:
+        prev = ""
+    else:
+        prev = file.read()
+        file.close()
 
-print(prev)
+    return prev != avail
 
-if prev == fp:
-    print("same as it ever was")
-    exit(0)
 
-print("new!!!")
+def cache(avail):
+    """Cache available times."""
+    io.open("prev/%s.txt" % (RIDE), "w", encoding='utf8').write(avail).close()
 
-f = io.open("prev/%s.txt" % (RIDE), "w", encoding='utf8')
-f.write(fp)
-f.close()
 
-client = Client()
+def send_message(avail):
+    """Send a SMS message with available times."""
+    client = Client()
 
-message_body = """FP+ found!
-%s, %s
+    message_body = """FP+ found!
+    %s, %s
 
-%s
-https://disneyworld.disney.go.com/fastpass-plus/""" % (RIDE, DATE, fp)
+    %s
+    https://disneyworld.disney.go.com/fastpass-plus/""" % (RIDE, DATE, avail)
 
-client.messages.create(to=os.environ['TO_PHONE'],
-                       from_=os.environ['FROM_PHONE'],
-                       body=message_body)
+    client.messages.create(to=os.environ['TO_PHONE'],
+                           from_=os.environ['FROM_PHONE'],
+                           body=message_body)
 
-print("message sent!")
+    print("message sent!")
+
+
+def main():
+    """Find available FP times and notify user if they changed."""
+    print("""STARTING SEARCH
+    Park: %s
+    Ride: %s
+    Date: %s
+    Current time: %s""" % (PARK, RIDE, DATE, str(datetime.now())))
+
+    avail = fetch_avail()
+
+    print(avail)
+
+    if not did_change(avail):
+        print("same as it ever was")
+        exit(0)
+
+    print("new!!!")
+
+    cache(avail)
+
+
+if __name__ == "__main__":
+    main()
